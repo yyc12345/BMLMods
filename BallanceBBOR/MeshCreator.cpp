dd0bfbb3b8175d17c91229b936e90b2e18ea3416
yyc12345 <yyc12321@outlook.com>
2023-11-19 16:56:34 +0800

    finish 2 type mesh creator

--- BallanceBBOR/MeshCreator.cpp
+++ BallanceBBOR/MeshCreator.cpp
@@ -2,11 +2,98 @@
 
 namespace NSBallanceBBOR::MeshCreator {
 
+#pragma region Help Functions
+
 	constexpr float YYC_PI = PI;
 	constexpr float YYC_2PI = YYC_PI * 2.0f;
 
-	void CreateSphere(CKMesh* mesh, CKMaterial* mtl, CKDWORD segments, CKDWORD rings, float radius) {
+	/**
+	 * @brief A circular index with very limited usage but have high performance.
+	 * @param index[int] The index to compute circular index. It should not minus than zero or greater than `(2 * max_index) - 1`
+	 * because this function only can handle only at least 1 loop.
+	 * @param max_index[in] The max index value. Can not <= 0.
+	 * @return
+	*/
+	static int NotRobustCircularIndex(int index, int max_index) {
+		if (index < max_index) return index;
+		else return index - max_index;
+	}
+
+	/**
+	 * @brief Force;y notify mesh its data changed.
+	 * @param mesh[in] Mesh to be notified.
+	*/
+	static void OrderUpdateMesh(CKMesh* mesh) {
+		mesh->NormalChanged();
+		mesh->UVChanged();
+	}
+
+#pragma endregion
+
+	void CreatePlane(CKMesh* mesh, CKMaterial* mtl, float size) {
+		// create cahce and normal (+Y)
+		VxVector universal_normal(0.0f, 1.0f, 0.0f);
 		VxVector cache;
+		// regulate size
+		if (size <= 0.0f) size = 5.0f;
+
+		// create 4 vector.
+		// we created plane is in XZ plane.
+		// we use p ad positive(+) and n for negtative(-), so pxpz is mean the point in +x +y.
+		// resize first
+		int vertex_base = mesh->GetVertexCount();
+		mesh->SetVertexCount(vertex_base + 4);
+
+		// set index and assign
+		int pxpz_idx = vertex_base,
+			nxpz_idx = vertex_base + 1,
+			nxnz_idx = vertex_base + 2,
+			pxnz_idx = vertex_base + 3;
+
+		cache.Set(size, 0.0f, size);
+		mesh->SetVertexPosition(pxpz_idx, &cache);
+		mesh->SetVertexNormal(pxpz_idx, &universal_normal);
+		mesh->SetVertexTextureCoordinates(pxpz_idx, 0.0f, 0.0f);
+	
+		cache.Set(-size, 0.0f, size);
+		mesh->SetVertexPosition(nxpz_idx, &cache);
+		mesh->SetVertexNormal(nxpz_idx, &universal_normal);
+		mesh->SetVertexTextureCoordinates(nxpz_idx, 0.0f, 0.0f);
+		
+		cache.Set(-size, 0.0f, -size);
+		mesh->SetVertexPosition(nxnz_idx, &cache);
+		mesh->SetVertexNormal(nxnz_idx, &universal_normal);
+		mesh->SetVertexTextureCoordinates(nxnz_idx, 0.0f, 0.0f);
+		
+		cache.Set(size, 0.0f, -size);
+		mesh->SetVertexPosition(pxnz_idx, &cache);
+		mesh->SetVertexNormal(pxnz_idx, &universal_normal);
+		mesh->SetVertexTextureCoordinates(pxnz_idx, 0.0f, 0.0f);
+
+		// create 2 face.
+		int face_base = mesh->GetFaceCount();
+		mesh->SetFaceCount(face_base + 2);
+		int pxpz_face_idx = face_base,
+			nxnz_face_idx = face_base + 1;
+
+		mesh->SetFaceVertexIndex(pxpz_face_idx, pxpz_idx, nxpz_idx, pxnz_idx);
+		mesh->SetFaceMaterial(pxpz_face_idx, mtl);
+
+		mesh->SetFaceVertexIndex(nxnz_face_idx, nxnz_idx, pxnz_idx, nxpz_idx);
+		mesh->SetFaceMaterial(nxnz_face_idx, mtl);
+
+		// notify changes
+		OrderUpdateMesh(mesh);
+	}
+
+	void CreateSphere(CKMesh* mesh, CKMaterial* mtl, int segments, int rings, float radius) {
+		// create cahce
+		VxVector cache;
+		// regulate segments and rings
+		if (segments < 3) segments = 3;
+		if (rings < 3) rings = 3;
+		// regulate radius
+		if (radius <= 0.0f) radius = 5.0f;
 
 		// get vertex size and resize
 		int vertex_base = mesh->GetVertexCount();
@@ -23,7 +110,7 @@ namespace NSBallanceBBOR::MeshCreator {
 		cache.Normalize();
 		mesh->SetVertexNormal(top_vtx_idx, &cache);
 		mesh->SetVertexTextureCoordinates(top_vtx_idx, 0.0f, 0.0f);
-		
+
 		cache.Set(0, -radius, 0);
 		mesh->SetVertexPosition(bottom_vtx_idx, &cache);
 		cache.Normalize();
@@ -58,18 +145,72 @@ namespace NSBallanceBBOR::MeshCreator {
 		}
 
 		// resize face count.
-		// each rectangle created by rings and segments will cause 2 face.
-		// we have "rings - 1" horizontal belt and "segments" vertical belt.
-		// so we have this face count:
+		// first, top and bottom face is pure triangle and have `2 * segments` faces.
+		// second, the amid between top and bottom face is the rectangle face created by rings and segments.
+		// each rectangle face will be splited into 2 triangle faces.
+		// and we have rectangle face `(rings - 2) * segments`
+		// so we have triangle face double of it.
 		int face_base = mesh->GetFaceCount();
-		mesh->SetFaceCount(face_base + ((rings - 1) * segments) * 2);
+		int top_face_count = segments,
+			bottom_face_count = segments,
+			other_face_count = ((rings - 2) * segments) * 2;
+		mesh->SetFaceCount(
+			face_base +
+			top_face_count + bottom_face_count +
+			other_face_count
+		);
+		int top_face_start = face_base,
+			bottom_face_start = face_base + top_face_count,
+			other_face_start = face_base + top_face_count + bottom_face_count;
 
-		// todo: create face
+		// create top bottom faces
+		for (int iseg = 0; iseg < segments; ++iseg) {
+			// compute line start
+			int top_line_start = other_vtx_start,
+				bottom_line_start = other_vtx_start + (rings - 1) * segments;
 
+			// compute vertex index
+			int top_line_this_vtx = top_line_start + iseg,
+				top_line_next_vtx = top_line_start + NotRobustCircularIndex(iseg + 1, segments);
+			int bottom_line_this_vtx = bottom_line_start + iseg,
+				bottom_line_next_vtx = bottom_line_start + NotRobustCircularIndex(iseg + 1, segments);
+
+			// create top face
+			mesh->SetFaceVertexIndex(top_face_start + iseg, top_vtx_idx, top_line_this_vtx, top_line_next_vtx);
+			mesh->SetFaceMaterial(top_face_start + iseg, mtl);
+
+			// create bottom face
+			mesh->SetFaceVertexIndex(bottom_face_start + iseg, bottom_vtx_idx, bottom_line_next_vtx, bottom_line_this_vtx);
+			mesh->SetFaceMaterial(bottom_face_start + iseg, mtl);
+		}
+
+		// create other faces
+		idx = other_face_start;
+		for (int iring = 0; iring < rings - 1; ++iring) {
+			// get ring start index
+			int this_line_start = other_vtx_start + (iring * segments),
+				next_line_start = other_vtx_start + ((iring + 1) * segments);
+
+			// loop segments
+			for (int iseg = 0; iseg < segments; ++iseg) {
+				// compute index
+				int this_line_this_vtx = this_line_start + iseg,
+					this_line_next_vtx = this_line_start + NotRobustCircularIndex(iseg + 1, segments);
+				int next_line_this_vtx = next_line_start + iseg,
+					next_line_next_vtx = next_line_start + NotRobustCircularIndex(iseg + 1, segments);
+
+				// create top-left triangle
+				mesh->SetFaceVertexIndex(idx, this_line_this_vtx, next_line_this_vtx, this_line_next_vtx);
+				mesh->SetFaceMaterial(idx, mtl);
+				++idx;
+				mesh->SetFaceVertexIndex(idx, this_line_next_vtx, next_line_this_vtx, next_line_next_vtx);
+				mesh->SetFaceMaterial(idx, mtl);
+				++idx;
+			}
+		}
 
 		// notify changes
-		mesh->NormalChanged();
-		mesh->UVChanged();
+		OrderUpdateMesh(mesh);
 	}
 
 }
--- BallanceBBOR/MeshCreator.h
+++ BallanceBBOR/MeshCreator.h
@@ -5,13 +5,21 @@
 namespace NSBallanceBBOR::MeshCreator {
 
 	/**
-	 * @brief Add a sphere shape for mesh.
-	 * @param mesh The mesh to add geomery.
+	 * @brief Add a plane (square rectangle) shape to mesh. The face normal will point to +Y.
+	 * @param mesh[in] The mesh to add geomery.
 	 * @param mtl[in] The mtl used for drawing this sphere.
-	 * @param segments The segment count of mesh, indicating how many vertical lines.
-	 * @param rings The ring count of mesh, indicating how many horizonal lines.
-	 * @param radius The radius of sphere.
+	 * @param size[in] The edge length of this square.
 	*/
-	void CreateSphere(CKMesh* mesh, CKMaterial* mtl, CKDWORD segments, CKDWORD rings, float radius);
+	void CreatePlane(CKMesh* mesh, CKMaterial* mtl, float size);
+
+	/**
+	 * @brief Add a sphere shape to mesh.
+	 * @param mesh[in] The mesh to add geomery.
+	 * @param mtl[in] The mtl used for drawing this sphere.
+	 * @param segments[in] The segment count of mesh, indicating how many vertical lines.
+	 * @param rings[in] The ring count of mesh, indicating how many horizonal lines.
+	 * @param radius[in] The radius of sphere.
+	*/
+	void CreateSphere(CKMesh* mesh, CKMaterial* mtl, int segments, int rings, float radius);
 
 }
